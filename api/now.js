4244f2e4b0112319d1ba86e937b805c36d32c1da
victorchiu419git <victorchiu419@gmail.com>
2025-08-15 18:29:10 +0800

    Create now.js

---
+++ api/now.js
@@ -0,0 +1,7 @@
+export const config = { runtime: 'edge' };
+
+export default async function handler() {
+  return new Response(JSON.stringify({ ok: true, ts: Date.now() }), {
+    status: 200, headers: { 'content-type': 'application/json' }
+  });
+}
