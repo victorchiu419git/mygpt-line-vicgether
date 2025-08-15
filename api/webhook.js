dea90467ec72fe23e3f78bc42b8a9e0a3a3ff937
victorchiu419git <victorchiu419@gmail.com>
2025-08-15 18:55:05 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,8 +1,1 @@
-// api/webhook.js
-export default async function handler(req, res) {
-  try {
-    return res.status(200).send('OK');
-  } catch (e) {
-    return res.status(200).send('OK');
-  }
-}
+export const config = { runtime: 'nodejs' }; // （可選）
